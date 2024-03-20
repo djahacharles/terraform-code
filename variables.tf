749e6bddfb1769f663ca9debc879f2d5bd2c105b
djahacharles <git config --global user.email>
2024-03-19 23:18:14 -0700

    Terraform

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "my_profile" {
+  default = "default"
+
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+
+variable "team" {
+  default = "DevOps"
+
+}
+
+variable "protocol_type" {
+  default = "email"
+
+}
+
+variable "my_endpoint" {
+  default = "kouassidjaha0@gmail.com"
+
+}
